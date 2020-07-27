b5fda0c9c4d95dd3f33566901d65840be50b261c
Maxim <maximMalofeev@bk.ru>
2020-07-27 16:24:50 +0300

    refactoring

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import datetime
 import time
 from firebase_admin import credentials, messaging, firestore
 
+
+def on_snap_in_danger_col(col_snap, changes, read_time):
+    for change in changes:
+        if change.type.name == "ADDED":
+            print(u'{} => {}, change type = {}'.format(
+                change.document.id, change.document.to_dict(), change.type))
+            owners = get_owners(change.document.id)
+            notify_owners(owners)
+            change.document.reference.delete()
+
+
 def get_owners(plate):
     db = firestore.client()
     plate_doc_ref = db.collection('plates').document(plate)
@@ -15,6 +26,7 @@ def get_owners(plate):
         print(f"No such plate({plate})")
         return []
 
+
 def notify_owners(owners):
     db = firestore.client()
     users_col_ref = db.collection('users')
@@ -23,10 +35,14 @@ def notify_owners(owners):
         user_snap = user_doc_ref.get()
         if user_snap.exists:
             user_dict = user_snap.to_dict()
-            print(f"Send notification to {owner} with name: {user_dict['name']}")
-            send_notification(title="Your car in danger", body="Harry up!", token=user_dict["registration_token"])
+            print(
+                f"Send notification to {owner} with name: {user_dict['name']}")
+            # TODO send multicast message
+            send_notification(title="Your car in danger",
+                              body="Harry up!", token=user_dict["registration_token"])
         else:
             print(f"User {owner} not exists any more")
+
 
 def send_notification(title, body, token):
     message = messaging.Message(
@@ -34,31 +50,23 @@ def send_notification(title, body, token):
             title=title, body=body),
         token=token,
     )
-    # Send a message to the device corresponding to the provided
-    # registration token.
-    response = messaging.send(message)
-    # Response is a message ID string.
-    print('Successfully sent message:', response)
 
-def on_snapshot(collection_snapshot, changes, read_time):
-    for change in changes:
-        if change.type.name == "ADDED":
-            print(u'{} => {}, change type = {}'.format(change.document.id, change.document.to_dict(), change.type))
-            owners = get_owners(change.document.id)
-            notify_owners(owners)
-            change.document.reference.delete()
+    response = messaging.send(message)
+    print('Successfully sent message:', response)
 
 
 if __name__ == "__main__":
     try:
-        cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+        cred = credentials.Certificate(
+            os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
         app = firebase_admin.initialize_app(cred)
-        
+
         print(app.name)
-        
+
         db = firestore.client()
-        col_watch = db.collection('in-danger').on_snapshot(on_snapshot)
-        
+        col_watch = db.collection(
+            'in-danger').on_snapshot(on_snap_in_danger_col)
+
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
